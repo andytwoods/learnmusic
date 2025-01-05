d960925e8efe0a3ac3ffe8a81829a0072f200909
andytwoods <andytwoods@gmail.com>
2025-01-05 14:33:33 +0000

    postgres

--- config/settings/production.py
+++ config/settings/production.py
@@ -20,18 +20,18 @@ DATABASES["default"]["CONN_MAX_AGE"] = env.int("CONN_MAX_AGE", default=60)
 
 # CACHES
 # ------------------------------------------------------------------------------
-CACHES = {
-    "default": {
-        "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": REDIS_URL,
-        "OPTIONS": {
-            "CLIENT_CLASS": "django_redis.client.DefaultClient",
-            # Mimicking memcache behavior.
-            # https://github.com/jazzband/django-redis#memcached-exceptions-behavior
-            "IGNORE_EXCEPTIONS": True,
-        },
-    },
-}
+#CACHES = {
+#    "default": {
+#        "BACKEND": "django_redis.cache.RedisCache",
+#        "LOCATION": REDIS_URL,
+#        "OPTIONS": {
+#            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+#            # Mimicking memcache behavior.
+#            # https://github.com/jazzband/django-redis#memcached-exceptions-behavior
+#            "IGNORE_EXCEPTIONS": True,
+#        },
+#    },
+#}
 
 # SECURITY
 # ------------------------------------------------------------------------------
