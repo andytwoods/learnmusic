ec4a84b8ed3383bdb85e24d948f047cae8e6baec
andytwoods <andytwoods@gmail.com>
2025-01-06 18:13:12 +0000

    setup_app command

--- config/management/commands/setup_app.py
+++ config/management/commands/setup_app.py
@@ -2,17 +2,18 @@ from django.contrib.auth import get_user_model
 from django.core.management.base import BaseCommand
 from django.utils.crypto import get_random_string
 
-User = get_user_model()
+from notes.models import Instrument
 
+User = get_user_model()
 
 class Command(BaseCommand):
     def handle(self, *args, **kwargs):
-        email = 'admin@example.com'
+        email = 'andytwoods@gmail.com'
         new_password = get_random_string(10)
         try:
             if not User.objects.filter(is_superuser=True).exists():
                 self.stdout.write("No superusers found, creating one")
-                User.objects.create_superuser(username='admin', email=email, password=new_password)
+                User.objects.create_superuser( email=email, password=new_password)
                 self.stdout.write("=======================")
                 self.stdout.write("A superuser has been created")
                 self.stdout.write("Username: admin")
@@ -20,6 +21,8 @@ class Command(BaseCommand):
                 self.stdout.write(f"Password: {new_password}")
 
                 self.stdout.write("=======================")
+
+                Instrument.create_default_instruments()
             else:
                 self.stdout.write("A superuser exists in the database. Skipping.")
         except Exception as e:
--- config/settings/base.py
+++ config/settings/base.py
@@ -92,6 +92,7 @@ THIRD_PARTY_APPS = [
 LOCAL_APPS = [
     "learnmusic.users",
     "notes.apps.NotesConfig",
+    "config",
 ]
 # https://docs.djangoproject.com/en/dev/ref/settings/#installed-apps
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
